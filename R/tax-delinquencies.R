bb3c7669f210b49d93432b27db9307ccca61572d
Nick Lucius <nicklucius@gmail.com>
2017-05-09 20:47:47 -0500

    Add tax delinquency data and alpha of feature fetcher.

---
+++ R/tax-delinquencies.R
@@ -0,0 +1,28 @@
+# ---------------------------------------------------------------------------- #
+#  Import all tax sale data
+# ---------------------------------------------------------------------------- #
+
+# annual_sale <-
+#   read.socrata("https://datacatalog.cookcountyil.gov/resource/bxb3-vf6m.json")
+# scavenger_sale <-
+#   read.socrata("https://datacatalog.cookcountyil.gov/resource/i7u4-3rbi.json")
+
+# ---------------------------------------------------------------------------- #
+#  Clean PINs
+# ---------------------------------------------------------------------------- #
+
+# annual_sale$pin <- gsub("-", "", annual_sale$pin)
+# annual_sale$pin <- str_sub(annual_sale$pin, 0, -5) # convert to 10 digit PINs
+# scavenger_sale$pin <- gsub("-", "", scavenger_sale$pin)
+# scavenger_sale$pin <- str_sub(scavenger_sale$pin, 0, -5) # convert to 10 digit PINs
+# tax_sales <- rbind(annual_sale[,c("pin", "tax_sale_year")],
+#                    scavenger_sale[c("pin", "tax_sale_year")])
+
+# ---------------------------------------------------------------------------- #
+#  Save/Read Rds File
+# ---------------------------------------------------------------------------- #
+
+
+# saveRDS(tax_sales, "data/tax-sales.Rds")
+tax_sales <- readRDS("data/tax-sales.Rds")
+
--- R/tax-parcels.R
+++ R/tax-parcels.R
@@ -2,23 +2,23 @@
 #  Import all Cook County tax parcel data
 # ---------------------------------------------------------------------------- #
 
-taxShapes <- readOGR(dsn="data/parcels.geojson", layer="OGRGeoJSON", stringsAsFactors = FALSE)
+# taxShapes <- readOGR(dsn="data/parcels.geojson", layer="OGRGeoJSON", stringsAsFactors = FALSE)
 
 # ---------------------------------------------------------------------------- #
 #  DRILL DOWN TO A FEW CENSUS TRACTS FOR FASTER COMPUTATION 
 # ---------------------------------------------------------------------------- #
 
-tracts <- readOGR(dsn="data/tracts.geojson", layer="OGRGeoJSON", stringsAsFactors = FALSE)
-# tractsList <- c("2424","2423","2422","2421","2420","2429","2430","2431","2432",
-#                 "2433")
-tractsList <- c("2431")
-tracts <- tracts[tracts$name10 %in% tractsList,]
-taxShapes_df <- over(taxShapes, tracts)
-taxShapes_df <- taxShapes_df[complete.cases(taxShapes_df),]
-rowNums <- as.numeric(rownames(taxShapes_df))
-taxShapes <- taxShapes[c(rowNums),]
-writeOGR(taxShapes, "data/taxShapes-small.geojson",layer="OGRGeoJSON",
-         driver="GeoJSON")
+# tracts <- readOGR(dsn="data/tracts.geojson", layer="OGRGeoJSON", stringsAsFactors = FALSE)
+# # tractsList <- c("2424","2423","2422","2421","2420","2429","2430","2431","2432",
+# #                 "2433")
+# tractsList <- c("2608")
+# tracts <- tracts[tracts$name10 %in% tractsList,]
+# taxShapes_df <- over(taxShapes, tracts)
+# taxShapes_df <- taxShapes_df[complete.cases(taxShapes_df),]
+# rowNums <- as.numeric(rownames(taxShapes_df))
+# taxShapes <- taxShapes[c(rowNums),]
+# writeOGR(taxShapes, "data/taxShapes-small.geojson",layer="OGRGeoJSON",
+#          driver="GeoJSON")
 
 # ---------------------------------------------------------------------------- #
 #  Read small geojson file
@@ -26,3 +26,5 @@ writeOGR(taxShapes, "data/taxShapes-small.geojson",layer="OGRGeoJSON",
 
 taxShapes <- readOGR("data/taxShapes-small.geojson", layer="OGRGeoJSON",
                      stringsAsFactors = FALSE)
+taxShapes <- spTransform(taxShapes, CRS("+init=epsg:4326"))
+
