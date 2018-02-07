24b3606a11c79759e4e551205492529f02b3b85f
nicklucius <nicholas.lucius2@cityofchicago.org>
2018-02-07 14:20:52 -0600

    Remove unnecessary/unused files

--- py/address-parser.py
+++
@@ -1,9 +0,0 @@
-import csv
-import usaddress
-import sys
-
-addr = str(sys.argv[1:])
-parsed = usaddress.parse(addr)
-with open("parsed.csv",'w') as resultFile:
-  wr = csv.writer(resultFile)
-  wr.writerows(parsed)
