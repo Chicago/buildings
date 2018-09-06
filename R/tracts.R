a1e043c4b35615f1d0ff1e06fb22cfa772c17b20
Tom Schenk Jr <tomschenkjr@users.noreply.github.com>
2018-09-06 13:44:01 -0500

    Specify "tracts" to avoid readOGR error
    When running this file, will encounter `Cannot open layer` error. Looks like the `layer` option was incorrect. This will be able to read the file.

--- R/tracts.R
+++ R/tracts.R
@@ -1,3 +1,3 @@
-tracts <- readOGR(dsn="data/geojson/tracts.geojson", layer="OGRGeoJSON",
+tracts <- readOGR(dsn="data/geojson/tracts.geojson", layer="tracts",
                   stringsAsFactors = FALSE)
 tracts <- spTransform(tracts, CRS("+init=epsg:4326"))
